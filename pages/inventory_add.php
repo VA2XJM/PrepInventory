48eec6ad696977bc965c38b3152480bdbe58ade3
Jean-Michel Vien <Xop777@gmail.com>
2017-01-06 15:44:59 -0500

    Display unit on inventory addition
    The unit of measure is added to the dropdown list when adding something
    to the inventory (FIX #33)

--- pages/inventory_add.php
+++ pages/inventory_add.php
@@ -130,7 +130,16 @@
 											if (mysqli_num_rows($result) < 1) { print ""; }
 											else {
 												while($row = mysqli_fetch_assoc($result)) {
-													print '<option value="'. $row['id'] .'">' . $row['name'] . '</option>';
+													$item_unit = $row['unit'];
+													$sql2 = "SELECT * FROM `inv_units` WHERE `id` = '$item_unit'";
+													$result2 = mysqli_query($link, $sql2);
+													if (mysqli_num_rows($result2) > 0) {
+														while($row2 = mysqli_fetch_assoc($result2)) {
+															$unit_name = $row2['name'];
+														}
+													}
+													else { $unit_name = "Unit"; }
+													print '<option value="'. $row['id'] .'">' . $row['name'] . ' ('.$unit_name.')</option>';
 												}
 											}
 										?>
