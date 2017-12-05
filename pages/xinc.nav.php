09ddd48ab6e656b7ace3f5160c2ef1ffeb2a5f64
Jean-Michel Vien <Xop777@gmail.com>
2017-12-05 16:26:45 -0500

    Search inline
    added the inline class to the search form to ensure it displays on a single line.

--- pages/xinc.nav.php
+++ pages/xinc.nav.php
@@ -105,7 +105,7 @@
 
 			<div class="navbar-default sidebar" role="navigation">
 				<div class="sidebar-nav navbar-collapse">
-					<ul class="nav" id="side-menu">
+					<ul class="nav form-inline" id="side-menu">
 						<li class="sidebar-search">
 							<div class="input-group custom-search-form">
 								<form role="form" method="post" action="search.php">
