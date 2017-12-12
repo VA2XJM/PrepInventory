e432b659357995998c7156447ddf7ad55c44dba4
Jean-Michel Vien <Xop777@gmail.com>
2017-12-12 06:36:38 -0500

    Reloading - Printable batch table
    a printable batch table has been added. It will be useful for range/bench references.

--- pages/reloading_batches_print.php
+++ pages/reloading_batches_print.php
@@ -10,31 +10,14 @@
 		die();
 	}
 
-	#
-	## Removing batch
-	#
-	if (!empty($_GET['delete'])) {
-		if (is_numeric($_GET['delete'])) {
-			$id = $_GET['delete'];
-			$sql = "DELETE FROM `reloading_batches` WHERE `id`='$id'";
-			$result = mysqli_query($link, $sql);
-
-			if ($result) {	$notice = '<div class="panel panel-success"><div class="panel-heading">Batch deleted.</div></div>'; }
-			else { $notice = '<div class="panel panel-danger"><div class="panel-heading">Error while deleting batch.</div></div>'; }
-		}
-		else { $notice = '<div class="panel panel-danger"><div class="panel-heading">Numerical value required.</div></div>'; }
-	}
-
+	include('xinc.config.php');
 
 	#
 	## Load and Display Batch
 	#
-	print '<h1>Batches</h1> Here is the actual list of batches that have not been shot yet. You can add new ones, delete existing ones or add grouping results.<br><b>Deleting a batch have no effects on data.</b>';
+	print '<h1>Batches</h1> Here is the actual list of batches that have not been shot yet.</b>';
 
-	if (isset($notice)) { print $notice; }
-
-	print '<br><br><a href="?page=batches_add">Create a new batch</a> - <a href="?page=batches_stats">View closed batches stats</a> - <a href="reloading_batches_print.php" target="_BLANK">Printable table</a>';
-	print '<table border="1" width="90%"><tr><th>Batch ID</th><th>Ammo Lot ID</th><th>Caliber</th><th>Bullet</th><th>Powder</th><th>Primer</th><th>Powder Charge</th><th width="20px">&nbsp;</th></tr>';
+	print '<table border="1" WIDTH="600"><tr><th>Batch ID</th><th>Lot ID</th><th>Caliber</th><th>Bullet</th><th>Powder</th><th>Primer</th><th>Powder Charge</th><th>Qty</th></tr>';
 	$sql = "SELECT *, t1.id AS `bid` FROM reloading_batches t1 LEFT JOIN reloading_data t2 ON t1.data = t2.id WHERE test_grouping IS NULL ORDER BY t1.id ASC";
 	$result = mysqli_query($link, $sql);
 	if (mysqli_num_rows($result) > 0) {
@@ -71,13 +54,12 @@
 			$resultx = mysqli_query($link, $sqlx);
 			if (mysqli_num_rows($resultx) > 0) { while($rowx = mysqli_fetch_assoc($resultx)) { $primer_name = $rowx['name']; } }
 
-			# Check if Ammo LOT is existing or not.
-			#  If exists show results link
-			#  Else show batching link
-			if (!empty($lot)) { $slink = '<a href="?page=batches_reload&id='. $row['bid'] .'" title="See reloading data"><i class="fa fa-magic fa-fw"></i></a><a href="?page=batches_results&id='. $row['bid'] .'" title="Input grouping results"><i class="fa fa-crosshairs fa-fw"></i></a>'; }
-			else { $slink = '<a href="?page=batches_assignlot&id='. $row['bid'] .'" title="Assign an ammo lot to this batch"><i class="fa fa-edit fa-fw"></i></a>'; }
+			# Shell lot qty
+			$sqlx = "SELECT * FROM reloading_shell_lots WHERE id = '$lot'";
+			$resultx = mysqli_query($link, $sqlx);
+			if (mysqli_num_rows($resultx) > 0) { while($rowx = mysqli_fetch_assoc($resultx)) { $shells = $rowx['qty']; } }
 
-			print '<tr><td>'.$row['bid'].'</td><td>'.$lot.'</td><td>'.$caliber_name.'</td><td>'.$bullet_name.'</td><td>'.$powder_name.'</td><td>'.$primer_name.'</td><td>'.$charge.' '.$powder_unit.'</td><td>'.$slink.' <a href="?page=batches&delete='. $row['bid'] .'" title="Delete"><i class="fa fa-minus-square fa-fw"></i></a></td></tr>';
+			print '<tr><td>'.$row['bid'].'</td><td>'.$lot.'</td><td>'.$caliber_name.'</td><td>'.$bullet_name.'</td><td>'.$powder_name.'</td><td>'.$primer_name.'</td><td>'.$charge.' '.$powder_unit.'</td><td>'.$shells.'</td></tr>';
 		}
 	}
 	print '</table>';
